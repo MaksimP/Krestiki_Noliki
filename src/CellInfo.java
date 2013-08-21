c8c7d09143fcd885b88949540f4b5ba3f5cd48fc
dev.nikor <dev.nikor@gmail.com>
2013-08-21 22:23:51 +0400

    maybe finished

--- src/CellInfo.java
+++ src/CellInfo.java
@@ -1,9 +1,37 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Nikita
- * Date: 21.08.13
- * Time: 18:55
- * To change this template use File | Settings | File Templates.
- */
+
+
 public class CellInfo {
+    private int x;
+    private int y;
+    private CellState figure;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Wrong parameter: x");
+        }
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        if (y < 0) {
+            throw new IllegalArgumentException("Wrong parameter: y");
+        }
+        this.y = y;
+    }
+
+    public CellState getFigure() {
+        return figure;
+    }
+
+    public void setFigure(CellState figure) {
+        this.figure = figure;
+    }
 }
