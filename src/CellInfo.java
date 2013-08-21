9aaa49b3d0af42daaacb4e0d801394100a3545ac
dev.nikor <dev.nikor@gmail.com>
2013-08-21 23:04:36 +0400

    all done

--- src/CellInfo.java
+++ src/CellInfo.java
@@ -6,9 +6,6 @@ public class CellInfo {
     private CellState figure;
 
     public CellInfo(int x, int y, CellState cellState) {
-        if (x < 0 && y < 0) {
-            throw new IllegalArgumentException("Wrong input parameter: ");
-        }
         X = x;
         Y = y;
         figure = cellState;
