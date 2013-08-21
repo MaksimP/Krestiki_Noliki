0e8c5ddde6821375fc4d542efc910b07ffd59fb2
nikkrichko <nik.krichko@ya.ru>
2013-08-21 19:04:50 +0300

    Созданы классы -- пора за работу.

---
+++ src/FieldController.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nikita
+ * Date: 21.08.13
+ * Time: 18:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class FieldController {
+}
---
+++ src/Player/Player.java
@@ -0,0 +1,13 @@
+package Player;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nikita
+ * Date: 21.08.13
+ * Time: 18:59
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Player {
+
+   void doMove (int x; int y);
+}
