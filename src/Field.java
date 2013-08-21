52ad242d3de06e873ed5f5e3f30fc4dcfaee9bf0
Evgeniy Dolgikh <marcondream@gmail.com>
2013-08-21 20:46:12 +0300

    Base implementation

--- src/Field.java
+++ src/Field.java
@@ -1,9 +1,34 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Nikita
- * Date: 21.08.13
- * Time: 18:54
- * To change this template use File | Settings | File Templates.
- */
 public class Field {
+    public final int SIZE;
+    private CellState[][] field;
+    private int availableCells = 0;
+    public Field(int fieldSize) {
+        SIZE = fieldSize;
+        field = new CellState[SIZE][SIZE];
+        clear();
+    }
+    public void setCell(CellInfo cell) {
+        if(cell.figure == CellState.EMPTY) {
+            availableCells++;
+        } else {
+            availableCells--;
+        }
+        field[cell.x][cell.y] = cell.figure;
+    }
+    public CellInfo getCell(CellInfo cell) {
+        return new CellInfo(cell.x, cell.y, field[cell.x][cell.y]);
+    }
+    public void clear() {
+        availableCells = 0;
+        for(int x = 0; x < SIZE; x++) {
+            for(int y = 0; y < SIZE; y++) {
+                setCell(new CellInfo(x,y,CellState.EMPTY));
+            }
+        }
+    }
+    public boolean isAvailible(CellInfo cell) {
+        if(field[cell.x][cell.y] == CellState.EMPTY)
+            return true;
+        return false;
+    }
 }
