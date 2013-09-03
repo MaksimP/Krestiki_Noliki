c5e5af58d81a602224a3ca09124f2f439523ea55
Evgeniy Dolgikh <marcondream@gmail.com>
2013-09-03 14:49:52 +0300

    rm unneeded classes move IPlayer interface to correct package and rename interface

--- src/main/java/org/hexlet/xo/player/Player.java
+++ src/main/java/org/hexlet/xo/player/Player.java
@@ -1,45 +1,7 @@
 package org.hexlet.xo.player;
 
-import org.hexlet.xo.CellInfo;
-import org.hexlet.xo.CellState;
-import org.hexlet.xo.Field;
-import org.hexlet.xo.Settings;
+public interface Player {
 
-public abstract class Player implements IPlayer{
-
-    public static final int MAX_SIZE_FIELD = 4;
-
-    public static final int ZERO = 0;
-
-    private String name;
-
-    private Settings settings;
-
-    private CellInfo cellInfo;
-
-    private CellState cellState;
-
-    private Field field;
-
-    protected Player(String name, CellState figure) {
-        this.name = name;
-        settings = new Settings(name);
-        cellState = figure;
-    }
-
-    protected boolean checkValue(int x, int y) {
-        boolean moreThanZero = x > ZERO && y > ZERO;
-        boolean lessThenMaxSize = x < MAX_SIZE_FIELD && y < MAX_SIZE_FIELD;
-        return moreThanZero && lessThenMaxSize;
-    }
-
-    @Override
-    public void doMove(int x, int y) {
-        cellInfo = new CellInfo(x, y, cellState);
-        if (checkValue(x, y) && this.field.isAvailable(cellInfo)) {
-            field.setCell(cellInfo);
-        }
-    }
+    void doMove (int x, int y);
 
 }
-
