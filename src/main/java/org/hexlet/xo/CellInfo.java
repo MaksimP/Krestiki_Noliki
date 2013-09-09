afb03f8dd891f0f22290aeb8b65c1f18751cbd64
Evgeniy Dolgikh <marcondream@gmail.com>
2013-09-09 12:43:31 +0300

    add coordinates check for CellInfo object

--- src/main/java/org/hexlet/xo/CellInfo.java
+++ src/main/java/org/hexlet/xo/CellInfo.java
@@ -1,11 +1,16 @@
 package org.hexlet.xo;
 
+import org.hexlet.xo.exception.InvalidCellCoordinatesException;
+
 public class CellInfo {
     public final int X;
     public final int Y;
     public final CellState figure;
 
-    public CellInfo(int x, int y, CellState cellState) {
+    public CellInfo(int x, int y, CellState cellState) throws InvalidCellCoordinatesException {
+        if (x < 0 || y < 0) {
+            throw new InvalidCellCoordinatesException("Cell coordinates can't be lower than zero");
+        }
         X = x;
         Y = y;
         figure = cellState;
