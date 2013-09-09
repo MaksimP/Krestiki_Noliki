c5fb9c6e35e9ed8ab3914e60c557f71ac9fbb353
Evgeniy Dolgikh <marcondream@gmail.com>
2013-09-09 12:40:51 +0300

    remove empty constructor

--- src/main/java/org/hexlet/xo/exception/CellNotAvailableException.java
+++ src/main/java/org/hexlet/xo/exception/CellNotAvailableException.java
@@ -2,10 +2,6 @@ package org.hexlet.xo.exception;
 
 public class CellNotAvailableException extends Exception {
 
-    public CellNotAvailableException() {
-        super();
-    }
-
     public CellNotAvailableException(String message) {
         super(message);
     }
